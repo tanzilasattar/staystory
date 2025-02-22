832adf7525ae956028bb5cdcabe8acf440fd5fa3
tanzilasattar <maliktanzela71@gmail.com>
2025-02-22 06:55:00 -0800

    navbar updates

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -8,93 +8,105 @@ const Navbar = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   return (
-    <nav className="w-full max-w-[1440px] h-[112px] mx-auto relative">
-      <div className="flex items-center justify-between h-full">
-        {/* logo section */}
-        <div className="absolute left-20 top-10">
-          <Link href="/">
-            <Image 
-              src="/images/logo.png" 
-              alt="Logo" 
-              width={110} 
-              height={31.15}
-              className="object-contain"
-            />
-          </Link>
-        </div>
-
-        {/* Navigation Links */}
-        <div className="hidden md:flex absolute left-[542px] top-[47px] w-[355px] h-[18px] items-center">
-          <div className="flex space-x-10"> 
-            <Link href="/our-work" className="text-gray-600 hover:text-gray-900">
-              our work
-            </Link>
-            <Link href="/about-us" className="text-gray-600 hover:text-gray-900">
-              about us
-            </Link>
-            <Link href="/join-us" className="text-gray-600 hover:text-gray-900">
-              join us
-            </Link>
-            <Link href="/press" className="text-gray-600 hover:text-gray-900">
-              press
+    <nav className="w-full bg-[#FAF7F0]">
+      <div className="max-w-[1440px] mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-[80px] lg:h-[112px]">
+          {/* Logo */}
+          <div className="flex-shrink-0">
+            <Link href="/">
+              <Image 
+                src="https://writeful.ly/wp-content/themes/writefully/writeful/img/logo.svg" 
+                alt="Logo" 
+                width={110} 
+                height={31.15}
+                className="object-contain"
+              />
             </Link>
           </div>
-        </div>
 
-        {/* Right Section*/}
-        <div className="hidden md:flex absolute right-20 top-10 w-[211px] h-8 items-center">
-          <div className="flex items-center space-x-6"> {/* gap: 24px */}
+          {/* Desktop Navigation - Show on medium and large screens */}
+          <div className="hidden md:flex items-center justify-center flex-1 ml-10">
+            <div className="flex space-x-6 lg:space-x-10">
+              <Link href="/our-work" className="text-gray-600 hover:text-gray-900">
+                our work
+              </Link>
+              <Link href="/about-us" className="text-gray-600 hover:text-gray-900">
+                about us
+              </Link>
+              <Link href="/join-us" className="text-gray-600 hover:text-gray-900">
+                join us
+              </Link>
+              <Link href="/press" className="text-gray-600 hover:text-gray-900">
+                press
+              </Link>
+            </div>
+          </div>
+
+          {/* Desktop Right Section - Show on medium and large screens */}
+          <div className="hidden md:flex items-center space-x-4 lg:space-x-6">
             <button className="p-2 hover:bg-gray-100 rounded-full">
               <Search size={20} className="text-gray-600" />
             </button>
-            
             <Link 
               href="/login" 
               className="px-4 py-2 bg-black text-white rounded-full text-sm"
             >
               login
             </Link>
-            
             <Link 
               href="/signin" 
-              className="px-4 py-2 text-gray-600 hover:text-gray-900 text-sm whitespace-nowrap text-center"
+              className="px-4 py-2 text-gray-600 hover:text-gray-900 text-sm whitespace-nowrap"
             >
               sign in
             </Link>
           </div>
+
+          {/* Mobile Menu Button - Show only on small screens */}
+          <div className="md:hidden">
+            <button 
+              className="p-2 hover:bg-gray-200 rounded-full"
+              onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+            >
+              {isMobileMenuOpen ? (
+                <X size={24} className="text-gray-600" />
+              ) : (
+                <Menu size={24} className="text-gray-600" />
+              )}
+            </button>
+          </div>
         </div>
 
-        {/* Mobile Menu Button */}
-        <button 
-          className="md:hidden absolute right-4 top-10 p-2 hover:bg-gray-100 rounded-full"
-          onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
-        >
-          {isMobileMenuOpen ? (
-            <X size={24} className="text-gray-600" />
-          ) : (
-            <Menu size={24} className="text-gray-600" />
-          )}
-        </button>
-
-        {/* Mobile Menu */}
+        {/* Mobile Menu - Show only on small screens when menu is open */}
         {isMobileMenuOpen && (
-          <div className="absolute top-[112px] left-0 right-0 bg-white shadow-lg md:hidden z-50">
-            <div className="flex flex-col p-4">
-              <Link href="/our-work" className="py-3 text-gray-600 hover:text-gray-900">
+          <div className="md:hidden bg-[#FAF7F0] border-t border-gray-200">
+            <div className="px-4 pt-2 pb-3 space-y-2">
+              <Link 
+                href="/our-work" 
+                className="block py-2 text-gray-600 hover:text-gray-900"
+              >
                 our work
               </Link>
-              <Link href="/about-us" className="py-3 text-gray-600 hover:text-gray-900">
+              <Link 
+                href="/about-us" 
+                className="block py-2 text-gray-600 hover:text-gray-900"
+              >
                 about us
               </Link>
-              <Link href="/join-us" className="py-3 text-gray-600 hover:text-gray-900">
+              <Link 
+                href="/join-us" 
+                className="block py-2 text-gray-600 hover:text-gray-900"
+              >
                 join us
               </Link>
-              <Link href="/press" className="py-3 text-gray-600 hover:text-gray-900">
+              <Link 
+                href="/press" 
+                className="block py-2 text-gray-600 hover:text-gray-900"
+              >
                 press
               </Link>
               
-              <div className="flex items-center space-x-4 pt-4 mt-4 border-t">
-                <button className="p-2 hover:bg-gray-100 rounded-full">
+              <div className="mt-4 pt-4 border-t border-gray-200 flex items-center space-x-4">
+                <button className="p-2 hover:bg-gray-200 rounded-full">
                   <Search size={20} className="text-gray-600" />
                 </button>
                 <Link 
